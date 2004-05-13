45635582a7a3e4a83b135baad794744bc1c123e6
carl <carl.codere@optimasc.com>
2004-05-13 23:05:19 +0000

    + add tests for ietf unit   + more tests for ISO Date/time conversion

--- tst/testit.pas
+++ tst/testit.pas
@@ -12,6 +12,8 @@ uses dpautils,
      vpautils,
      fpautils,
      tpautils,
+     locale,
+     ietf,
      unicode,
      dos
      ;
@@ -119,6 +121,96 @@ uses dpautils,
    WriteLn(s);
   end;
   
+  procedure testisvalidisodatestring;
+  begin
+   if not IsValidIsoDateString('1998') then
+     Runerror(255);
+   if IsValidIsoDateString(' 998') then
+     Runerror(255);
+   if IsValidIsoDateString('C') then
+     Runerror(255);
+   if IsValidIsoDateString('20056') then
+     Runerror(255);
+   if IsValidIsoDateString('1998/06/12') then
+     Runerror(255);
+   if not IsValidIsoDateString('1998-02') then
+     Runerror(255);
+   if IsValidIsoDateString('1998-2') then
+     Runerror(255);
+   if not IsValidIsoDateString('19980201') then
+     Runerror(255);
+   if not IsValidIsoDateString('1998-02-01') then
+     Runerror(255);
+  end;
+  
+  procedure testisvalidisotimestring;
+  begin
+   if not IsValidIsoTimeString('00:59:59') then
+     RunError(255);
+   if IsValidIsoTimeString('25:59:59') then
+     RunError(255);
+   if IsValidIsoTimeString('00Z59:59') then
+     RunError(255);
+   if IsValidIsoTimeString('00:59Z59') then
+     RunError(255);
+   if IsValidIsoTimeString('00:59:77') then
+     RunError(255);
+   if not IsValidIsoTimeString('00:59:59+00:30') then
+     RunError(255);
+   if IsValidIsoTimeString('00:59:59+ZZ:30') then
+     RunError(255);
+   if IsValidIsoTimeString('00:59:59W00:30') then
+     RunError(255);
+   if not IsValidIsoTimeString('00:59:59-01:00') then
+     RunError(255);
+   if not IsValidIsoTimeString('00:59:59Z') then
+     RunError(255);
+   if not IsValidIsoTimeString('005959') then
+     RunError(255);
+   if not IsValidIsoTimeString('005959') then
+     RunError(255);
+   if not IsValidIsoTimeString('005959Z') then
+     RunError(255);
+   if not IsValidIsoTimeString('00:59') then
+     RunError(255);
+   if not IsValidIsoTimeString('0059') then
+     RunError(255);
+     
+  end;
+  
+
+  procedure testisvalidisodatetimestring;
+  begin
+   if not IsValidIsoDateTimeString('1998-02-01T00:59:59') then
+     RunError(255);
+  end;
+
+  procedure testurn;
+  begin
+   if urn_isvalid('urn:@34') then
+     RunError(255);
+   if not urn_isvalid('urn:34-4567:345') then
+     RunError(255);
+   if urn_isvalid('urn:3:345%%') then
+     RunError(255);
+   if urn_isvalid('urn::345') then
+     RunError(255);
+   if not urn_isvalid('urn:ISBN:345-4567') then
+     RunError(255);
+   if not urn_isvalid('urn:ISBN:345-%ff4567') then
+     RunError(255);
+  end;
+
+  procedure testmime;
+  begin
+    if not mime_isvalidcontenttype('audio/x-test') then
+      RunError(255);
+    if mime_isvalidcontenttype('audio-x-test') then
+      RunError(255);
+    if mime_isvalidcontenttype('audio') then
+      RunError(255);
+  end;
+
 
 var
   s: string;
@@ -148,8 +240,16 @@ Begin
   testreadutf8;
   testreadutf16le;
   testreadcp850;
+  testisvalidisodatestring;
+  testisvalidisotimestring;
+  testisvalidisodatetimestring;
+  testurn;
+  testmime;
 end.
 
 {
   $Log: not supported by cvs2svn $
+  Revision 1.2  2004/05/06 15:29:55  carl
+    + tests for unicode conversion
+
 }
