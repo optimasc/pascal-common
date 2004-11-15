4746054630ee1ed9896d23f2aeb4fd596ba466e5
carl <carl.codere@optimasc.com>
2004-11-15 03:34:03 +0000

    * alternate names are now individually parsed

--- src/iso639.pas
+++ src/iso639.pas
@@ -1,6 +1,6 @@
 {
  ****************************************************************************
-    $Id: iso639.pas,v 1.1 2004-08-19 00:21:16 carl Exp $
+    $Id: iso639.pas,v 1.2 2004-11-15 03:34:03 carl Exp $
     Copyright (c) 2004 by Carl Eric Codere
 
     Language code unit
@@ -45,36 +45,72 @@ uses
 }
 function isvalidlangcode(s: shortstring): boolean;
 
-{ This routine returns the language name in french 
+{** This routine returns the language name in french 
   for the specified language code. The language code
   IS case insensitive and can be either 2 or 3 characters
   in length (according to ISO 639-1 and ISO 639-2 respectively)
   
   The returned string is encoded according to
-  ISO-8859-1.
+  ISO-8859-1. If there are alternate names for
+  the language, only the first alternate 
+  name is returned.
   
   @param(s The two or three digit language code)
   
 }
 function getlangname_fr(s: shortstring): shortstring;
 
-{ This routine returns the language name in english
+{** This routine returns the language name in english
   for the specified language code. The language code
   IS case insensitive and can be either 2 or 3 characters
-  in length (according to ISO 639-1 and ISO 639-2 respectively)
+  in length (according to ISO 639-1 and ISO 639-2 respectively).
   
   The returned string is encoded according to
-  ISO-8859-1.
+  ISO-8859-1.If there are alternate names for
+  the language, only the first alternate 
+  name is returned.
   
   @param(s The two or three digit language code)
 }
 function getlangname_en(s: shortstring): shortstring;
 
 
+{** This routine returns the 2 character code related
+    to the english name of the language. The search is
+    not case  (according to ISO 639-1). If there is
+    no 2 character language code for this language, or
+    if the language name is not found, the routine 
+    returns an empty string.
+  
+    The language name string should be encoded according to
+    ISO-8859-1.
+  
+  @param(name The name of the language)
+  @returns(The 2 character language code)
+}
+function getlangcode_en(name: shortstring): shortstring;
+
+{** This routine returns the 2 character code related
+    to the french name of the language. The search is
+    not case  (according to ISO 639-1). If there is
+    no 2 character language code for this language, or
+    if the language name is not found, the routine 
+    returns an empty string.
+  
+    The language name string should be encoded according to
+    ISO-8859-1.
+  
+  @param(name The name of the language)
+  @returns(The 2 character language code)
+}
+function getlangcode_fr(name: shortstring): shortstring;
+
+
 
 implementation
 
-{$I iso639.inc}
+uses iso639d;
+
 
 type
     tlangsets =  array[1..MAX_ENTRIES] of tlanginfo;
@@ -86,8 +122,24 @@ var
     ExitSave: pointer;
 {$ifdef tp}    
     p: pchar;
-{$endif}    
+{$endif}
 
+
+   {** this routine is used to clean a a language
+       name, it removes any extra information (that
+       is in parenthesis), as well as removing all
+       extra whitespace }
+   Function CleanName(s: string): string;
+   var
+    index: integer;
+   begin
+    index:=Pos('(',s);
+    if index <> 0 then
+      begin
+        delete(s,index,pos(')',s));
+      end;
+    CleanName:=trim(s);
+   end;
 
 
 function isvalidlangcode(s: shortstring): boolean;
@@ -116,6 +168,8 @@ end;
 function getlangname_fr(s: shortstring): shortstring;
 var
  i: integer;
+ index: integer;
+ name: string;
 begin
   getlangname_fr:='';
   if (length(s) > 3) or (length(s) = 0) then
@@ -125,14 +179,24 @@ begin
       { Check the 2-digit code }
       if Lang_Info^[i].code = s then
          begin
-            getlangname_fr:=Lang_Info^[i].name_fr;
+            index:=pos(';',Lang_Info^[i].name_fr);
+            if index<>0 then
+                name:=CleanName(copy(Lang_Info^[i].name_fr,1,index-1))
+            else
+                name:=CleanName(Lang_Info^[i].name_fr);
+            getlangname_fr:=name;
             exit;
          end
       else
       { Check the 3-digit code }
       if Lang_Info^[i].biblio_code = s then
          begin
-            getlangname_fr:=Lang_Info^[i].name_fr;
+            index:=pos(';',Lang_Info^[i].name_fr);
+            if index<>0 then
+                name:=CleanName(copy(Lang_Info^[i].name_fr,1,index-1))
+            else
+                name:=CleanName(Lang_Info^[i].name_fr);
+            getlangname_fr:=name;
             exit;
          end;
     end;
@@ -141,6 +205,8 @@ end;
 function getlangname_en(s: shortstring): shortstring;
 var
  i: integer;
+ index: integer;
+ name: string;
 begin
   getlangname_en:='';
   if (length(s) > 3) or (length(s) = 0) then
@@ -150,18 +216,99 @@ begin
       { Check the 2-digit code }
       if Lang_Info^[i].code = s then
          begin
-            getlangname_en:=Lang_Info^[i].name_en;
+            index:=pos(';',Lang_Info^[i].name_en);
+            if index<>0 then
+                name:=CleanName(copy(Lang_Info^[i].name_en,1,index-1))
+            else
+                name:=CleanName(Lang_Info^[i].name_en);
+            getlangname_en:=name;
             exit;
          end
       else
       { Check the 3-digit code }
       if Lang_Info^[i].biblio_code = s then
          begin
-            getlangname_en:=Lang_Info^[i].name_en;
+            index:=pos(';',Lang_Info^[i].name_en);
+            if index<>0 then
+                name:=CleanName(copy(Lang_Info^[i].name_en,1,index-1))
+            else
+                name:=CleanName(Lang_Info^[i].name_en);
+            getlangname_en:=name;
             exit;
          end;
     end;
 end;
+
+function getlangcode_en(name: shortstring): shortstring;
+var
+ i: integer;
+ name_en: string;
+ s: string;
+ index: integer;
+begin
+  getlangcode_en:='';
+  for i:=1 to MAX_ENTRIES do
+    begin
+      name_en:=CleanName(Lang_Info^[i].name_en);
+      repeat
+        index:=pos(';',name_en);
+        if index<>0 then
+         begin
+          s:=copy(name_en,1,index-1);
+          delete(name_en,1,index);
+         end
+        else
+          s:=name_en;
+
+         { Now check if there are several names for the same language,
+           each alternate name is separated by the others with a ;
+           character.
+         }
+         { Check the 2-digit code }
+         if upstring(trim(s)) = upstring(name) then
+         begin
+            getlangcode_en:=Lang_Info^[i].code;
+            exit;
+         end;
+      until (index=0);
+    end;
+end;
+
+function getlangcode_fr(name: shortstring): shortstring;
+var
+ i: integer;
+ name_fr: string;
+ s: string;
+ index: integer;
+begin
+  getlangcode_fr:='';
+  for i:=1 to MAX_ENTRIES do
+    begin
+      name_fr:=CleanName(Lang_Info^[i].name_fr);
+      repeat
+        index:=pos(';',name_fr);
+        if index<>0 then
+         begin
+          s:=copy(name_fr,1,index-1);
+          delete(name_fr,1,index);
+         end
+        else
+          s:=name_fr;
+
+         { Now check if there are several names for the same language,
+           each alternate name is separated by the others with a ;
+           character.
+         }
+         { Check the 2-digit code }
+         if upstring(trim(s)) = upstring(name) then
+         begin
+            getlangcode_fr:=Lang_Info^[i].code;
+            exit;
+         end;
+      until (index=0);
+    end;
+end;
+
 
 
 procedure releaseresources;far;
@@ -186,4 +333,7 @@ end.
 
 {
   $Log: not supported by cvs2svn $
+  Revision 1.1  2004/08/19 00:21:16  carl
+    + Language code unit
+
 }
