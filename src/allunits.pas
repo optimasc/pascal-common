76347017319f5ee3c1e8ba916f329e05b9adbbac
carl <carl.codere@optimasc.com>
2007-02-08 04:56:23 +0000

    * Added some missing units

--- src/allunits.pas
+++ src/allunits.pas
@@ -1,12 +1,16 @@
-program allunits;
+unit allunits;
+
+interface
 
 uses
+  dos,
   collects,
   crc,
   ctypes,
   dateutil,
   dpautils,
   fpautils,
+  gpautils,
   fs,
   vpautils,
   tpautils,
@@ -14,14 +18,26 @@ uses
   unicode,
   iso639,
   iso3166,
+  strsrch,
   ietf,
   sgml,
+  fileio,
 {$IFDEF LINUX}
 {$IFDEF CPU86}
   posix,
 {$ENDIF}  
 {$ENDIF}
+{$IFDEF WIN32}
+  extdos,
+{$ENDIF}
+{$IFDEF LINUX}
+{$IFDEF CPU86}
+  extdos,
+{$ENDIF}  
+{$ENDIF}
   utils;
+  
+implementation  
   
 
 begin
