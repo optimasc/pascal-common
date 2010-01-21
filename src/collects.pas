c5c787fe739bc93501ad2ec22f9d136ef2603602
carl <carl.codere@optimasc.com>
2010-01-21 12:01:09 +0000

    + Add TKeyValueCollection class

--- src/collects.pas
+++ src/collects.pas
@@ -1,5 +1,5 @@
 {
-    $Id: collects.pas,v 1.6 2005-07-20 03:14:39 carl Exp $
+    $Id: collects.pas,v 1.7 2010-01-21 12:01:09 carl Exp $
     Copyright (c) 2004 by Carl Eric Codere
 
     Collections (Object style)
@@ -25,7 +25,8 @@ uses
   vpautils,
   fpautils,
   tpautils,
-  dpautils;
+  dpautils,
+  utils;
 
 
 
@@ -106,7 +107,7 @@ TYPE
    PExtendedCollection = ^TExtendedCollection;
 
 {---------------------------------------------------------------------------}
-{          TExtendedSortedCollection OBJECT - SORTED COLLECTION ANCESTOR            }
+{          TExtendedSortedCollection OBJECT - SORTED COLLECTION ANCESTOR    }
 {---------------------------------------------------------------------------}
 TYPE
    {** @abstract(Base sorted collection object) }
@@ -148,11 +149,138 @@ TYPE
   TLongintCollection = object(TExtendedCollection)
     procedure FreeItem(Item: pointer); virtual;
   end;
-   
+  
+  
+  const KEY_NO_FASTKEY = high(word);
+
+  type
+  TKeyType = (typ_element, typ_array);
+  PKeyValueArrayItems = ^TKeyValueArrayItems;
+  PKeyValueCollection = ^TKeyValueCollection;
+
+  PKeyValue = ^TKeyValue;  
+  TKeyValue = record
+    key: pshortstring;
+    fastkey: word;
+    qualifiers: PKeyValueCollection;
+    case typ:TKeyType of
+     {** Contains an individual attribute }
+     typ_element: (value: pchar);
+     {** Contains a SEQ, ALT or BAG }
+     typ_array: (collection : PKeyValueCollection);
+  end;
+
+
+  TKeyValueArrayItems = object(TExtendedCollection)
+    procedure FreeItem(Item: pointer); virtual;
+  end;
+
+
+  TKeyValueCollection = Object(TExtendedSortedCollection)
+      CONSTRUCTOR Init (ALimit, ADelta: Integer);
+      Destructor done; virtual;
+      FUNCTION Compare (Key1, Key2: Pointer): Integer;            Virtual;
+      PROCEDURE FreeItem (Item: Pointer); Virtual;
+
+      {** Returns the property value associated with this key string. If the
+          property does not exist or is a type array of values,
+          this routine returns nil }
+      function getProperty(ns,key: string): pchar;
+      {** Returns the property value associated with this key index.
+          If the property does not exist or is a type array of values,
+          this routine returns nil. It returns the first value that has
+          this fastkey value.
+      }
+      function getPropertyByFastIndex(fastkey: integer): pchar;
+      {** Returns the qualifier associated with this key string. If the
+          property does not exist or is a type array of values,
+          this routine returns nil, or if the qualifier does not exist,
+          the returned value is also nil. }
+      function getPropertyQualifier(ns,key: string; qualNS, qualName: string): pchar;
+
+
+      {** Sets the property value to associate with this key and fastkey.
+
+          It shall overwrite any other property with the same name and shall
+          return the old value associated with this property (possibly truncated).
+          If this property did not exist or if trying to set a value to
+          an array of values, the return value will be nil.
+
+          WARNING: There is no verification to determine if the fastKey value is
+          not already allocated in this key collection. The fastKey value can be from
+          0..65534.
+
+          The return value should be copied, since it may be destroyed
+          by another call to this method.
+
+          @param(key Key name associated with this value)
+          @param(fastkey Numeric identifier associated with this value, 65535 (KEY_NO_FASTKEY) if no fastKey association)
+      }
+      function setProperty(ns,key: string; fastkey: integer; value: pchar): pchar;
+      {** Sets a qualifier to a specified element property.
+      }
+      function setPropertyQualifier(ns,key, qualNS, qualName: string; value: pchar): pchar;
+
+
+      {** Deletes the specified property. This method deletes
+          the specified property, which can either be a simply property or
+          an array, in which it cases all its sub-elements are also deleted.
+
+          @param(key Key name to delete)
+          @returns(true if successfull deletion, otherwise false such as is
+            this property does not exist)
+      }
+
+      function deleteProperty(ns,key: string): boolean;
+
+      {** Deletes the specified qualifier of the specified property. }
+      function deletePropertyQualifier(ns,key: string; qualNS, qualName: string): boolean;
+(*
+      {** Deletes the specified array item of the specified property }
+      function deletePropertyArrayItem(key: string; index: integer): boolean;
+*)
+      {** Adds a value to a list of values of a specified key. If
+          the array does not exist, it is first created.
+
+          @param(key This is the key associated with this array)
+          @param(value  Actual value of the element in the array list)
+          @returns(The index of this element in the array)
+      }
+      function appendPropertyArrayItem(ns,key: string; value: pchar): integer;
+      {** Returns the number of items in the value array of a specified
+          key. Returns -1 if this key does not exist. If the value exists, 
+          but is not array type, this will always return 1.
+      }
+      function getPropertyArrayItemCount(ns,key: string): integer;
+      {** Returns the specified item in the value array at the specified index,
+          starting from index 0. If this is not an array type key, and the
+          key exists, it will always retun that key value, ignoring the
+          index parameter. Returns nil if the key does not exist.
+      }
+      function getPropertyArrayItem(ns,key: string;index: integer): pchar;
+(*      
+      function setPropertyArrayItem(key: string;index:integer; value: pchar): pchar;
+*)      
+
+      function setPropertyArrayItemQualifier(ns, key: string; qualNS, qualName: string; index: integer; value: pchar): pchar;
+      function getPropertyArrayItemQualifier(ns, key: string; qualNS, qualName: string; index: integer): pchar;
+
+
+      {** Returns the key name associated with the specified index, the index starting at 0 to count -1. The
+          returns an empty string if out of bounds. The index is an internal value and is not
+          the same as the fastKey index.
+      }
+      function getKeyName(index: integer): string;
+   private
+      returnedProperty: pchar;
+      function getIndex(key: string): integer;
+      function getFastKeyIndex(fastkey: integer): integer;
+   END;
+
 
 Implementation
 
-uses utils;
+uses strings;
 
 
      procedure TLongintCollection.FreeItem(Item: pointer);
@@ -566,6 +694,444 @@ Begin
 end;
 
 
+{+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++}
+{                     TKeyValueCollection OBJECT METHODS                    }
+{+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++}
+FUNCTION TKeyValueCollection.Compare (Key1, Key2: Pointer): integer;
+VAR I, J: integer; P1, P2: PShortString;
+BEGIN
+   P1 := PShortString(PKeyValue(Key1)^.key);                               { String 1 pointer }
+   P2 := PShortString(PKeyValue(Key2)^.Key);                               { String 2 pointer }
+   If (Length(P1^)<Length(P2^)) Then J := Length(P1^)
+     Else J := Length(P2^);                           { Shortest length }
+   I := 1;                                            { First character }
+   While (I<J) AND (P1^[I]=P2^[I]) Do Inc(I);         { Scan till fail }
+   If (I=J) Then Begin                                { Possible match }
+   { * REMARK * - Bug fix   21 August 1997 }
+     If (P1^[I]<P2^[I]) Then Compare := -1 Else       { String1 < String2 }
+       If (P1^[I]>P2^[I]) Then Compare := 1 Else      { String1 > String2 }
+       If (Length(P1^)>Length(P2^)) Then Compare := 1 { String1 > String2 }
+         Else If (Length(P1^)<Length(P2^)) Then       { String1 < String2 }
+           Compare := -1 Else Compare := 0;           { String1 = String2 }
+   { * REMARK END * - Leon de Boer }
+   End Else If (P1^[I]<P2^[I]) Then Compare := -1     { String1 < String2 }
+     Else Compare := 1;                               { String1 > String2 }
+END;
+
+destructor TKeyValueCollection.done;
+begin
+  if assigned(returnedProperty) then
+    strdispose(returnedProperty);
+  Inherited Done;
+
+end;
+
+
+
+PROCEDURE TKeyValueCollection.FreeItem (Item: Pointer);
+var
+ p: PKeyValue;
+BEGIN
+   p:=PKeyValue(Item);
+   if assigned(p) then
+     begin
+        if p^.typ = typ_element then
+          begin
+           if assigned(p^.value) then
+             strdispose(p^.value);
+          end
+        else
+          begin
+            if assigned(p^.collection) then
+               Dispose(p^.collection,done);
+          end;
+       if assigned(p^.key) then
+         stringdispose(p^.key);
+       if assigned(p^.qualifiers) then
+         begin
+           Dispose(p^.qualifiers, done);
+           p^.qualifiers:=nil;
+         end;
+       dispose(p);
+     end;
+END;
+
+
+{** This returns the index in the collection of the element
+    associated with this key, or -1 if not found.
+}
+function TKeyValueCollection.getIndex(key: string): integer;
+var
+ KeyValue: PkeyValue;
+ i: integer;
+begin
+  getIndex:=-1;
+  if count > 0 then
+    begin
+      for i:=0 to count - 1 do
+        begin
+           KeyValue:=PKeyValue(At(i));
+           if KeyValue^.Key^ = key then
+             begin
+                getIndex:=i;
+                exit;
+             end;
+        end;
+    end;
+end;
+
+
+{** This returns the index in the collection of the element
+    associated with this fast key value, or -1 if not found.
+}
+function TKeyValueCollection.getFastKeyIndex(fastkey: integer): integer;
+var
+ KeyValue: PkeyValue;
+ i: integer;
+begin
+  getFastKeyIndex:=-1;
+  if fastKey = KEY_NO_FASTKEY then
+    exit;
+  if count > 0 then
+    begin
+      for i:=0 to count - 1 do
+        begin
+           KeyValue:=PKeyValue(At(i));
+           { Always skip elements with no FAST ID }
+           if KeyValue^.FastKey = KEY_NO_FASTKEY then
+             continue;
+           if KeyValue^.FastKey = fastkey then
+             begin
+                getFastKeyIndex:=i;
+                exit;
+             end;
+        end;
+    end;
+end;
+
+
+
+function TKeyValueCollection.getKeyName(index: integer): string;
+var
+ KeyValue: PKeyValue;
+begin
+  getKeyName:='';
+  if (index < 0) or (index > count-1) then
+    exit;
+  KeyValue:=PKeyValue(At(index));
+  getKeyName:=KeyValue^.key^;
+
+  end;
+
+
+{** This returns the property associated with this key. If there
+    is no such key, then return nil.
+}
+function TKeyValueCollection.getProperty(ns,key: string): pchar;
+var
+ idx: integer;
+ KeyValue: PKeyValue;
+begin
+  getProperty:=nil;
+  idx:=getIndex(key);
+  if idx = -1 then
+    exit;
+  KeyValue:=PKeyValue(At(idx));
+  if KeyValue^.typ <> typ_element then
+    exit;
+  getProperty:=KeyValue^.value;
+end;
+
+
+{** This returns the property associated with this index. If there
+    is no such key, then return nil.
+}
+function TKeyValueCollection.getPropertyByFastIndex(fastkey: integer): pchar;
+var
+ idx: integer;
+ KeyValue: PKeyValue;
+begin
+  getPropertyByFastIndex:=nil;
+  idx:=getFastKeyIndex(fastkey);
+  if idx = -1 then
+    exit;
+  KeyValue:=PKeyValue(At(idx));
+  if KeyValue^.typ <> typ_element then
+    exit;
+  getPropertyByFastIndex:=KeyValue^.value;
+end;
+
+
+function TKeyValueCollection.setProperty(ns,key: string;fastkey: integer; value: pchar): pchar;
+var
+ Prop: pchar;
+ attr: shortstring;
+ KeyValue: PKeyValue;
+ idx: integer;
+begin
+  setProperty:=nil;
+  idx:=getIndex(key);
+  if idx <> -1 then
+    begin
+      KeyValue:=PKeyValue(At(idx));
+      { This is an array of values, so you are using the wrong API }
+      if KeyValue^.typ = typ_array then
+        exit;
+      if assigned(returnedProperty) then
+        begin
+          strdispose(returnedProperty);
+          returnedProperty:=nil;
+        end;
+      returnedProperty:=strnew(KeyValue^.value);
+      setProperty:=returnedProperty;
+      Free(At(idx));
+    end;
+  new(KeyValue);
+  KeyValue^.key:=stringdup(key);
+  KeyValue^.value:=strnew(value);
+  KeyValue^.typ:=typ_element;
+  KeyValue^.Qualifiers:=nil;
+  KeyValue^.fastkey:=FastKey;
+  Insert(KeyValue);
+end;
+
+function TKeyValueCollection.appendPropertyArrayItem(ns,key: string; value: pchar): integer;
+var
+ KeyArray: PKeyValue;
+ KeyValue: PKeyValue;
+ idx: integer;
+begin
+  appendPropertyArrayItem:=-1;
+  idx:=getIndex(key);
+  { Not allowed to recreate an array that already exists }
+  if idx <> -1 then
+   begin
+     KeyArray:=PKeyValue(At(idx));
+   end
+  else
+   begin
+     new(KeyArray);
+     KeyArray^.typ:=typ_array;
+     KeyArray^.key:=stringdup(key);
+     KeyArray^.Qualifiers:=nil;
+     KeyArray^.FastKey:=KEY_NO_FASTKEY;
+     New(KeyArray^.collection,init(32,32));
+     Insert(KeyArray);
+   end;
+  new(KeyValue);
+  KeyValue^.key:=stringdup(key);
+  KeyValue^.value:=strnew(value);
+  KeyValue^.typ:=typ_element;
+  KeyValue^.Qualifiers:=nil;
+  KeyArray^.collection^.Insert(KeyValue);
+  { The index is always the last element in the collection }
+  appendPropertyArrayItem:=KeyArray^.collection^.count - 1;
+end;
+
+
+
+function TKeyValueCollection.getPropertyArrayItemCount(ns,key: string): integer;
+var
+ idx:integer;
+ KeyValue: PKeyValue;
+begin
+  getPropertyArrayItemCount:=-1;
+  idx:=getIndex(key);
+  { Not found, then simply return -1 }
+  if idx = -1 then
+    exit;
+  KeyValue:=PKeyValue(At(idx));
+  if KeyValue^.typ=typ_element then
+    begin
+      getPropertyArrayItemCount:=1;
+      exit;
+    end;
+  getPropertyArrayItemCount:=KeyValue^.collection^.Count;
+end;
+
+
+function TKeyValueCollection.getPropertyArrayItem(ns,key: string;index: integer): pchar;
+var
+ idx:integer;
+ KeyValue: PKeyValue;
+ KeyElement: PKeyValue;
+begin
+  getPropertyArrayItem:=nil;
+  idx:=getIndex(key);
+  { Not found, then simply return nil }
+  if idx = -1 then
+    exit;
+  KeyValue:=PKeyValue(At(idx));
+  { Return always the property if this is not an array element }
+  if KeyValue^.typ=typ_element then
+    begin
+      getPropertyArrayItem:=getProperty(ns,key);
+      exit;
+    end;
+  if index > (KeyValue^.collection^.Count-1) then
+    exit;
+  KeyElement:=KeyValue^.collection^.at(index);
+  getPropertyArrayItem:=KeyElement^.value;
+end;
+
+(*
+function TKeyValueCollection.setPropertyArrayItem(key: string;index:integer; value: pchar): pchar;
+begin
+end;*)
+
+function TKeyValueCollection.deleteProperty(ns, key : string):Boolean;
+ var
+  idx: integer;
+ begin
+  deleteProperty:=false;
+  idx:=getIndex(key);
+  { Not found, then simply return nil }
+  if idx = -1 then
+    exit;
+  AtFree(idx);
+  deleteProperty:=true;  
+ end;
+
+(*
+function TKeyValueCollection.deletePropertyArrayItem(key: string; index: integer):Boolean;
+ begin
+ end;*)
+
+function TKeyValueCollection.setPropertyQualifier(ns,key, qualNS, qualName: string; value: pchar): pchar;
+var
+ Prop: pchar;
+ KeyValue: PKeyValue;
+ idx: integer;
+begin
+  setPropertyQualifier:=nil;
+  idx:=getIndex(key);
+  if idx = -1 then
+    exit;
+  KeyValue:=PKeyValue(At(idx));
+
+  if not assigned(KeyValue^.Qualifiers) then
+    New(KeyValue^.Qualifiers, init(1,4));
+  setPropertyQualifier:=keyValue^.Qualifiers^.setProperty(qualNS,qualName,KEY_NO_FASTKEY,value);
+end;
+
+
+function TKeyValueCollection.getPropertyQualifier(ns,key: string; qualNS, qualName: string): pchar;
+var
+ Prop: pchar;
+ KeyValue: PKeyValue;
+ idx: integer;
+begin
+  getPropertyQualifier:=nil;
+  idx:=getIndex(key);
+  if idx = -1 then
+    exit;
+  KeyValue:=PKeyValue(At(idx));
+  if not assigned(KeyValue^.Qualifiers) then
+    exit;
+  getPropertyQualifier:=keyValue^.Qualifiers^.getProperty(qualNS,qualName);
+end;
+
+
+function TKeyValueCollection.deletePropertyQualifier(ns,key: string; qualNS, qualName: string): boolean;
+var
+ Prop: pchar;
+ KeyValue: PKeyValue;
+ idx: integer;
+begin
+  deletePropertyQualifier:=false;
+  idx:=getIndex(key);
+  if idx = -1 then
+    exit;
+  KeyValue:=PKeyValue(At(idx));
+  if not assigned(KeyValue^.Qualifiers) then
+    exit;
+  deletePropertyQualifier:=keyValue^.Qualifiers^.deleteProperty(qualNS,qualName);
+end;
+
+
+
+function TKeyValueCollection.setPropertyArrayItemQualifier(ns, key: string; qualNS, qualName: string; index: integer; value: pchar): pchar;
+var
+ idx:integer;
+ KeyValue: PKeyValue;
+ KeyElement: PKeyValue;
+begin
+  setPropertyArrayItemQualifier:=nil;
+  idx:=getIndex(key);
+  { Not found, then simply return nil }
+  if idx = -1 then
+    exit;
+  KeyValue:=PKeyValue(At(idx));
+  { Return always the property if this is not an array element }
+  if KeyValue^.typ=typ_element then
+    begin
+      exit;
+    end;
+  if index > (KeyValue^.collection^.Count-1) then
+    exit;
+  KeyElement:=KeyValue^.collection^.at(index);
+  KeyValue^.collection^.SetPropertyQualifier('',KeyElement^.value,qualNS,qualName,value);
+end;
+
+
+function TKeyValueCollection.getPropertyArrayItemQualifier(ns, key: string; qualNS, qualName: string; index: integer): pchar;
+var
+ idx:integer;
+ KeyValue: PKeyValue;
+ KeyElement: PKeyValue;
+begin
+  getPropertyArrayItemQualifier:=nil;
+  idx:=getIndex(key);
+  { Not found, then simply return nil }
+  if idx = -1 then
+    exit;
+  KeyValue:=PKeyValue(At(idx));
+  { Return always the property if this is not an array element }
+  if KeyValue^.typ=typ_element then
+    begin
+      exit;
+    end;
+  if index > (KeyValue^.collection^.Count-1) then
+    exit;
+  KeyElement:=KeyValue^.collection^.at(index);
+  KeyValue^.collection^.GetPropertyQualifier('',KeyElement^.value,qualNS,qualName);
+end;
+
+
+
+CONSTRUCTOR TKeyValueCollection.Init (ALimit, ADelta: Integer);
+Begin
+  Inherited Init(ALimit,ADelta);
+end;
+
+{+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++}
+{                              TKeyValueCollection                          }
+{+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++}
+
+
+procedure TKeyValueArrayItems.FreeItem(Item: pointer);
+var
+ p: PKeyValue;
+BEGIN
+   p:=PKeyValue(Item);
+   if assigned(p) then
+     begin
+        if p^.typ = typ_element then
+          begin
+           if assigned(p^.value) then
+             strdispose(p^.value);
+          end
+        else
+          begin
+            if assigned(p^.collection) then
+               Dispose(p^.collection,done);
+          end;
+       if assigned(p^.key) then
+         stringdispose(p^.key);
+       dispose(p);
+     end;
+END;
+
 
 {*****************************************************************************
                                  Stack
@@ -626,6 +1192,9 @@ end;
 End.
 {
   $Log: not supported by cvs2svn $
+  Revision 1.6  2005/07/20 03:14:39  carl
+   + Added TLongintCollection
+
   Revision 1.5  2004/11/19 01:37:25  carl
     + more documentation
 
