88fe485839cbe88e1ed5526894bf32aca6a1cb62
carl <carl.codere@optimasc.com>
2012-10-24 15:29:22 +0000

    + Add new units

--- src/allunits.pas
+++ src/allunits.pas
@@ -17,18 +17,8 @@ uses
   ietf,
   sgml,
   fileio,
-{$IFDEF LINUX}
-{$IFDEF CPU86}
-  posix,
-{$ENDIF}  
-{$ENDIF}
 {$IFDEF WIN32}
   extdos,
-{$ENDIF}
-{$IFDEF LINUX}
-{$IFDEF CPU86}
-  extdos,
-{$ENDIF}  
 {$ENDIF}
   cmnutils;
   
