55a455493274314c8d78d6be588c353f49a6c947
carl <carl.codere@optimasc.com>
2012-10-24 14:53:44 +0000

    + According to new units

--- tools/cvtlang.pas
+++ tools/cvtlang.pas
@@ -9,10 +9,7 @@
 {$X+}
 program cvtlang;
 
-uses
-  fpautils,
-  tpautils,
-  unicode,
+uses cmntyp,unicode,
   utils,
   dos,locale;
 
@@ -20,12 +17,11 @@ uses
 
 const
   MAX_LINES = 1024;
-  MAXLENGTH = 64;
   
 type
   TLangRecord = record
-    name_fr: string[maxlength];
-    name_en: string[maxlength];
+    name_fr: string;
+    name_en: string;
     biblio_code: string[3];
     terminology_code: string[3];
     code: string[2];
@@ -34,6 +30,23 @@ type
 
 var
  Entries: array[1..MAX_LINES] of TLangRecord;
+ 
+ 
+{** Converts any character after 127 to a character literal representation }
+function ToByte(c: char): string;
+ var
+  b: byte;
+ Begin
+   if ord(c) > 127 then
+     Begin
+       b:=ord(c);
+       ToByte:=''''+'#'+decStr(b,0)+'''';
+     end
+   else
+     Begin
+       ToByte:=c;
+     end
+ end;
 
 
 var
@@ -45,14 +58,17 @@ var
  name_en,name_fr: string;
  idxpos: integer;
  T: Text;
+ k: integer;
  maxentries: integer;
  duplicate: boolean;
+ maxCurLength: integer;
 Begin
  GetDate(Year,Month,Day,DayOfWeek);
  Fillchar(Entries,sizeof(entries),#0);
  { Start reading the english text file }
- Assign(EnFile,'U:\users\data\carl\source\common\iso-63~1.txt');
+ Assign(EnFile,'iso-63~1.txt');
  Reset(EnFile);
+ maxCurLength:=0;
  i:=1;
  Repeat
    Readln(EnFile,s);
@@ -69,8 +85,16 @@ Begin
        delete(s,1,idxpos);
        idxpos := pos('|',s);
        name_en:=copy(s,1,idxpos-1);
+       if length(name_en) > maxCurLength then
+         Begin
+           maxCurLength:=length(name_en);
+         end;
        delete(s,1,idxpos);
        name_fr:=s;
+       if length(name_fr) > maxCurLength then
+         Begin
+           maxCurLength:=length(name_fr);
+         end;
 
        { Check if there are already entries with this code,
          if so don't add them
@@ -97,7 +121,7 @@ Begin
  maxentries:=i-1;
  Close(Enfile);
  { Now write the PASCAL file }
- Assign(T,'U:\users\data\carl\source\common\src\iso639d.pas');
+ Assign(T,'iso639d.pas');
  Rewrite(T);
  S:=GetISODateString(Year, Month, Day);
  WriteLn(T,'{ File Generated on ',S,' automatically }');
@@ -110,10 +134,10 @@ Begin
  WriteLn(T,'  MAX_ENTRIES =',maxentries,';');
  WriteLn(T,'type');
  WriteLn(T,'  TLangInfo = record');
- WriteLn(T,'    name_fr: string[',maxlength,'];');
- WriteLn(T,'    name_en: string[',maxlength,'];');
  WriteLn(T,'    code: string[2];');
  WriteLn(T,'    biblio_code: string[3];');
+ WriteLn(T,'    name_fr: pchar;');
+ WriteLn(T,'    name_en: pchar');
  WriteLn(T,'  end;');
  WriteLn(T);
  WriteLn(T);
@@ -123,6 +147,8 @@ Begin
  for i:=1 to maxentries do
    begin
      WriteLn(T,'  (');
+     WriteLn(T,'   code: ''',Entries[i].code,''';');
+     WriteLn(T,'   biblio_code: ''',Entries[i].biblio_code,''';');
      Write(T,'   name_fr: ''');
      for idxpos:=1 to length(Entries[i].Name_fr) do
         begin
@@ -132,7 +158,7 @@ Begin
                Write(T,'''');
                Write(T,'''');
             end;
-            Write(T,Entries[i].Name_fr[idxpos]);
+            Write(T,ToByte(Entries[i].Name_fr[idxpos]));
         end;
      WriteLn(T,''';');   
      Write(T,'   name_en: ''');
@@ -144,12 +170,9 @@ Begin
                Write(T,'''');
                Write(T,'''');
             end;
-            Write(T,Entries[i].Name_en[idxpos]);
+            Write(T,ToByte(Entries[i].Name_en[idxpos]));
         end;
      WriteLn(T,''';');   
-     WriteLn(T,'   code: ''',Entries[i].code,''';');
-     WriteLn(T,'   biblio_code: ''',Entries[i].biblio_code,'''');
-
      if i <> maxentries then
        WriteLn(T,'  ),')
      else
@@ -169,7 +192,7 @@ Begin
  WriteLn(T,'end.');
  Close(T);
  { Now write the Assembler file }
- Assign(T,'U:\users\data\carl\source\common\src\iso639.asm');
+ Assign(T,'iso639.asm');
  Rewrite(T);
  S:=GetISODateString(Year, Month, Day);
  WriteLn(T,'; File Generated on ',S,' automatically ');
@@ -178,40 +201,62 @@ Begin
  WriteLn(T);
  WriteLn(T,'.MODEL LARGE, PASCAL');
  WriteLn(T,'.CODE');
+
+ WriteLn(T,'langcodesdata PROC FAR');
+ WriteLn(T,'PUBLIC langcodesdata');
+ 
+ for i:=1 to maxentries do
+   begin
+     WriteLn(T,';-------------------------------------------------------');
+     
+     WriteLn(T,'; rec[',i,'].name_fr');
+     WriteLn(T,'langNameFr',i,':');
+     k:=0;
+     for j:=1 to length(Entries[i].Name_fr) do
+     begin
+       if ((k mod 16) = 0) then
+       begin
+         WriteLn(T);
+         Write(T,' DB ');
+       end;
+       if ((k mod 16) <> 0) then
+         Write(T,',');
+       Write(T,hexstr(ord(Entries[i].Name_fr[j]),3),'h');
+       inc(k);
+     end;
+     Write(T,',00h');
+     WriteLn(T);
+     
+     
+     WriteLn(T,'; rec[',i,'].name_en');
+     WriteLn(T,'langNameEn',i,':');
+     
+     k:=0;
+     for j:=1 to length(Entries[i].Name_En) do
+     begin
+       if ((k mod 16) = 0) then
+       begin
+         WriteLn(T);
+         Write(T,' DB ');
+       end;
+       if ((k mod 16) <> 0) then
+         Write(T,',');
+       Write(T,hexstr(ord(Entries[i].Name_En[j]),3),'h');
+       inc(k);
+     end;
+     Write(T,',00h');
+     WriteLn(T);
+   end;
+ WriteLn(T);
+ WriteLn(T,'langcodesdata ENDP');
+ 
+ 
  WriteLn(T,'langcodesproc PROC FAR');
  WriteLn(T,'PUBLIC langcodesproc');
  
  for i:=1 to maxentries do
    begin
      WriteLn(T,';-------------------------------------------------------');
-     WriteLn(T,'; rec[',i,'].name_fr[',maxlength,']');
-     
-     for j:=0 to (maxlength) do
-     begin
-       if ((j mod 16) = 0) then
-       begin
-         WriteLn(T);
-         Write(T,' DB ');
-       end;
-       if ((j mod 16) <> 0) then
-         Write(T,',');
-       Write(T,hexstr(ord(Entries[i].Name_fr[j]),3),'h');
-     end;
-     WriteLn(T);
-     WriteLn(T,'; rec[',i,'].name_en[',maxlength,']');
-     
-     for j:=0 to (maxlength) do
-     begin
-       if ((j mod 16) = 0) then
-       begin
-         WriteLn(T);
-         Write(T,' DB ');
-       end;
-       if ((j mod 16) <> 0) then
-         Write(T,',');
-       Write(T,hexstr(ord(Entries[i].Name_en[j]),3),'h');
-     end;
-     WriteLn(T);
      
      WriteLn(T);
      WriteLn(T,'; rec[',i,'].code[2]');
@@ -246,10 +291,19 @@ Begin
      WriteLn(T);
      
      
+     WriteLn(T,'; rec[',i,'].name_fr');
      
+     WriteLn(T,' DD ','langNameFr',i);
+
+     WriteLn(T,'; rec[',i,'].name_en');
+     
+     WriteLn(T,' DD ','langNameEn',i);
+     WriteLn(T);
    end;
  WriteLn(T);
  WriteLn(T,'langcodesproc ENDP');
  WriteLn(T,'END');
+ 
+ 
  Close(T);
 end.
