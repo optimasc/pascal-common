35e37759dc8173ea1d94eb722591d80c49307c93
carl <carl.codere@optimasc.com>
2014-11-29 16:45:57 +0000

    + Added endian manipulation routines + generic defines directory + generic fpc configuration file

---
+++ src/endian.pas
@@ -0,0 +1,418 @@
+unit endian;
+
+interface
+
+uses classes;
+
+function readbe16(buffer: pansichar; offset: longint): word;
+function readle16(buffer: pansichar; offset: longint): word;
+function readbe32(buffer: pansichar; offset: longint): longword;
+function readle32(buffer: pansichar; offset: longint): longword;
+function readle64(buffer: pansichar; offset: longint): int64;
+function readbe64(buffer: pansichar; offset: longint): int64;
+function readlef32(buffer: pansichar; offset: longint): single;
+function readbef32(buffer: pansichar; offset: longint): single;
+function readlef64(buffer: pansichar; offset: longint): double;
+function readbef64(buffer: pansichar; offset: longint): double;
+function readoctet(buffer: pansichar; offset: longint): byte;
+
+
+function hosttobe32(value: longword): longword;
+function hosttole32(value: longword): longword;
+function hosttobe16(value: word): word;
+function hosttole16(value: word): word;
+
+function be32tohost(value: longword): longword;
+function le32tohost(value: longword): longword;
+function be16tohost(value: word): word;
+function le16tohost(value: word): word;
+
+
+procedure writebe32(buffer: pansichar; offset: longint; value: longword);
+procedure writele32(buffer: pansichar; offset: longint; value: longword);
+procedure writebe16(buffer: pansichar; offset: longint; value: word);
+procedure writele16(buffer: pansichar; offset: longint; value: word);
+procedure writeoctet(buffer: pansichar; offset: longint; value: byte);
+
+procedure writelef32(buffer: pansichar; offset: longint; value: single);
+procedure writebef32(buffer: pansichar; offset: longint; value: single);
+procedure writelef64(buffer: pansichar; offset: longint; value: double);
+procedure writebef64(buffer: pansichar; offset: longint; value: double);
+
+implementation
+
+var
+ big_endian: boolean;
+ pansicharptr: pansichar;
+
+function readbe32(buffer: pansichar; offset: longint): longword;
+ begin
+     readbe32:= (ord(buffer[offset]) shl 24) or ((ord(buffer[offset + 1]) and $ff) shl 16)
+        or ((ord(buffer[offset + 2]) and $ff) shl 8) or (ord(buffer[offset + 3]) and $ff);
+ end;
+
+
+function readlef32(buffer: pansichar; offset: longint): single;
+ var 
+   value: single;
+   longValue: longword;
+ begin
+   Assert(sizeof(single) = 4);
+   longValue:=readle32(buffer,offset);
+   Move(longValue,value,sizeof(value));
+   readlef32:=value;
+ end;
+ 
+function readbef32(buffer: pansichar; offset: longint): single;
+ var
+   value: single;
+   longValue: longword;
+ begin
+   Assert(sizeof(single) = 4);
+   longValue:=readbe32(buffer,offset);
+   Move(longValue,value,sizeof(value));
+   readbef32:=value;
+ end;
+ 
+function readle32(buffer: pansichar; offset: longint): longword;
+ var
+  lw: longword;
+ begin
+    readle32:= (ord(buffer[offset + 3]) shl 24) or ((ord(buffer[offset + 2]) and $ff) shl 16)
+       or ((ord(buffer[offset + 1]) and $ff) shl 8) or (ord(buffer[offset]) and $ff);
+ end;
+  
+function readbe16(buffer: pansichar; offset: longint): word;
+ begin
+   readbe16 :=  (ord(buffer[offset]) and $ff) shl 8 or (ord(buffer[offset + 1]) and $ff);
+ end;
+ 
+function readle16(buffer: pansichar; offset: longint): word;
+ begin
+   readle16 :=  (ord(buffer[offset + 1]) and $ff) shl 8 or (ord(buffer[offset]) and $ff);
+ end;
+ 
+function readoctet(buffer: pansichar; offset: longint): byte;
+ begin
+   readoctet:=byte(buffer[offset]);
+ end;
+ 
+function readle64(buffer: pansichar; offset: longint): int64;
+ var
+   outbuf: array[0..7] of byte;
+   value: int64;
+ Begin
+         outbuf[0] := byte(buffer[offset+0]);
+         outbuf[1] := byte(buffer[offset+1]);
+         outbuf[2] := byte(buffer[offset+2]);
+         outbuf[3] := byte(buffer[offset+3]);
+         outbuf[4] := byte(buffer[offset+4]);
+         outbuf[5] := byte(buffer[offset+5]);
+         outbuf[6] := byte(buffer[offset+6]);
+         outbuf[7] := byte(buffer[offset+7]);
+   Move(outbuf,value,sizeof(outbuf));
+   readle64:=value;
+ end;
+
+function readbe64(buffer: pansichar; offset: longint): int64;
+ var
+   outbuf: array[0..7] of byte;
+   value: int64;
+ Begin
+         outbuf[0] := byte(buffer[offset+7]);
+         outbuf[1] := byte(buffer[offset+6]);
+         outbuf[2] := byte(buffer[offset+5]);
+         outbuf[3] := byte(buffer[offset+4]);
+         outbuf[4] := byte(buffer[offset+3]);
+         outbuf[5] := byte(buffer[offset+2]);
+         outbuf[6] := byte(buffer[offset+1]);
+         outbuf[7] := byte(buffer[offset+0]);
+   Move(outbuf,value,sizeof(outbuf));
+   readbe64:=value;
+ end;
+
+function readlef64(buffer: pansichar; offset: longint): double;
+ Begin
+ end;
+ 
+function readbef64(buffer: pansichar; offset: longint): double;
+ var
+   outbuf: array[0..7] of byte;
+   value: double;
+ Begin
+  if big_endian then
+      Begin
+         outbuf[0] := byte(buffer[offset+0]);
+         outbuf[1] := byte(buffer[offset+1]);
+         outbuf[2] := byte(buffer[offset+2]);
+         outbuf[3] := byte(buffer[offset+3]);
+         outbuf[4] := byte(buffer[offset+4]);
+         outbuf[5] := byte(buffer[offset+5]);
+         outbuf[6] := byte(buffer[offset+6]);
+         outbuf[7] := byte(buffer[offset+7]);
+      end
+    else
+      Begin
+         outbuf[0] := byte(buffer[offset+7]);
+         outbuf[1] := byte(buffer[offset+6]);
+         outbuf[2] := byte(buffer[offset+5]);
+         outbuf[3] := byte(buffer[offset+4]);
+         outbuf[4] := byte(buffer[offset+3]);
+         outbuf[5] := byte(buffer[offset+2]);
+         outbuf[6] := byte(buffer[offset+1]);
+         outbuf[7] := byte(buffer[offset+0]);
+      end;
+   Move(outbuf,value,sizeof(value));
+   readbef64:=value;
+ end;
+ 
+
+function hosttobe32(value: longword): longword;
+  var
+    y : word;
+    z : word;
+ begin
+   if big_endian then
+     hosttobe32:=value
+   else
+     begin
+       y := (value shr 16) and $FFFF;
+       y := word((y shl 8) or ((y shr 8) and $ff));
+       z := value and $FFFF;
+       z := word((z shl 8) or ((z shr 8) and $ff));
+       hosttobe32 := longword((longword(z) shl 16) or longword(y));
+     end;
+ end;
+
+function hosttole32(value: longword): longword;
+  var
+    y : word;
+    z : word;
+ begin
+   if big_endian=false then
+     hosttole32:=value
+   else
+     begin
+       y := (value shr 16) and $FFFF;
+       y := word((y shl 8) or ((y shr 8) and $ff));
+       z := value and $FFFF;
+       z := word((z shl 8) or ((z shr 8) and $ff));
+       hosttole32 := longword((longword(z) shl 16) or longword(y));
+     end;
+ end;
+
+function hosttobe16(value: word): word;
+ var
+  z: word;
+ begin
+   if big_endian then
+     hosttobe16:=value
+   else
+     begin
+      z := (value shr 8) and $ff;
+      value := value and $ff;
+      value := (value shl 8);
+      hosttobe16 := value or z;
+     end;
+ end;
+
+function hosttole16(value: word): word;
+ var
+  z: word;
+ begin
+   if big_endian=false then
+     hosttole16:=value
+   else
+     begin
+      z := (value shr 8) and $ff;
+      value := value and $ff;
+      value := (value shl 8);
+      hosttole16 := value or z;
+     end;
+ end;
+
+function be32tohost(value: longword): longword;
+var
+  y : word;
+  z : word;
+begin
+ if big_endian=true then
+   be32tohost:=value
+ else
+   begin
+     y := (value shr 16) and $FFFF;
+     y := word((y shl 8) or ((y shr 8) and $ff));
+     z := value and $FFFF;
+     z := word((z shl 8) or ((z shr 8) and $ff));
+     be32tohost := longword((longword(z) shl 16) or longword(y));
+   end;
+end;
+
+function le32tohost(value: longword): longword;
+var
+  y : word;
+  z : word;
+begin
+ if big_endian=false then
+   le32tohost:=value
+ else
+   begin
+     y := (value shr 16) and $FFFF;
+     y := word((y shl 8) or ((y shr 8) and $ff));
+     z := value and $FFFF;
+     z := word((z shl 8) or ((z shr 8) and $ff));
+     le32tohost := longword((longword(z) shl 16) or longword(y));
+   end;
+end;
+
+function be16tohost(value: word): word;
+var
+ z: word;
+ begin
+   if big_endian=true then
+     be16tohost:=value
+   else
+     begin
+      z := (value shr 8) and $ff;
+      value := value and $ff;
+      value := (value shl 8);
+      be16tohost := value or z;
+     end;
+ end;
+
+function le16tohost(value: word): word;
+var
+ z: word;
+ begin
+   if big_endian=false then
+     le16tohost:=value
+   else
+     begin
+      z := (value shr 8) and $ff;
+      value := value and $ff;
+      value := (value shl 8);
+      le16tohost := value or z;
+     end;
+ end;
+
+procedure writebe32(buffer: pansichar; offset: longint; value: longword);
+ begin
+  buffer[offset] := ansichar((value shr 24) and $ff);
+  buffer[offset+1] := ansichar((value shr 16) and $ff);
+  buffer[offset+2] := ansichar((value shr 8) and $ff);
+  buffer[offset+3] := ansichar(value and $ff);
+ end;
+ 
+procedure writele32(buffer: pansichar; offset: longint; value: longword);
+ begin
+   buffer[offset+3] := ansichar((value shr 24) and $ff);
+   buffer[offset+2] := ansichar((value shr 16) and $ff);
+   buffer[offset+1] := ansichar((value shr 8) and $ff);
+   buffer[offset] := ansichar(value and $ff);
+ end;
+ 
+procedure writebe16(buffer: pansichar; offset: longint; value: word);
+ begin
+   buffer[offset] := ansichar((value shr 8) and $ff);
+   buffer[offset+1] := ansichar((value) and $ff);
+ end;
+ 
+procedure writele16(buffer: pansichar; offset: longint; value: word);
+ begin
+   buffer[offset+1] := ansichar((value shr 8) and $ff);
+   buffer[offset] := ansichar((value) and $ff);
+ end;
+ 
+procedure writeoctet(buffer: pansichar; offset: longint; value: byte);
+ begin
+  buffer[offset] := ansichar(value);
+ end;
+
+procedure writelef32(buffer: pansichar; offset: longint; value: single);
+ var
+  lw: longword;
+ Begin
+   Move(value,lw,sizeof(lw));
+   writele32(buffer,offset,lw);
+ end;
+
+procedure writebef32(buffer: pansichar; offset: longint; value: single);
+ var
+  lw: longword;
+ Begin
+   Move(value,lw,sizeof(lw));
+   writele32(buffer,offset,lw);
+ end;
+
+procedure writelef64(buffer: pansichar; offset: longint; value: double);
+var
+  outbuf: array[0..7] of byte;
+ Begin
+  Move(value,outbuf,sizeof(value));
+  if big_endian then
+       Begin
+          buffer[offset+0] := ansichar(outbuf[0]);
+          buffer[offset+1] := ansichar(outbuf[1]);
+          buffer[offset+2] := ansichar(outbuf[2]);
+          buffer[offset+3] := ansichar(outbuf[3]);
+          buffer[offset+4] := ansichar(outbuf[4]);
+          buffer[offset+5] := ansichar(outbuf[5]);
+          buffer[offset+6] := ansichar(outbuf[6]);
+          buffer[offset+7] := ansichar(outbuf[7]);
+       end
+     else
+       Begin
+          buffer[offset+0] := ansichar(outbuf[7]);
+          buffer[offset+1] := ansichar(outbuf[6]);
+          buffer[offset+2] := ansichar(outbuf[5]);
+          buffer[offset+3] := ansichar(outbuf[4]);
+          buffer[offset+4] := ansichar(outbuf[3]);
+          buffer[offset+5] := ansichar(outbuf[2]);
+          buffer[offset+6] := ansichar(outbuf[1]);
+          buffer[offset+7] := ansichar(outbuf[0]);
+       end;
+ end;
+
+procedure writebef64(buffer: pansichar; offset: longint; value:double);
+var
+  outbuf: array[0..7] of byte;
+ Begin
+  Move(value,outbuf,sizeof(value));
+  if big_endian then
+       Begin
+          buffer[offset+0] := ansichar(outbuf[0]);
+          buffer[offset+1] := ansichar(outbuf[1]);
+          buffer[offset+2] := ansichar(outbuf[2]);
+          buffer[offset+3] := ansichar(outbuf[3]);
+          buffer[offset+4] := ansichar(outbuf[4]);
+          buffer[offset+5] := ansichar(outbuf[5]);
+          buffer[offset+6] := ansichar(outbuf[6]);
+          buffer[offset+7] := ansichar(outbuf[7]);
+       end
+     else
+       Begin
+          buffer[offset+0] := ansichar(outbuf[7]);
+          buffer[offset+1] := ansichar(outbuf[6]);
+          buffer[offset+2] := ansichar(outbuf[5]);
+          buffer[offset+3] := ansichar(outbuf[4]);
+          buffer[offset+4] := ansichar(outbuf[3]);
+          buffer[offset+5] := ansichar(outbuf[2]);
+          buffer[offset+6] := ansichar(outbuf[1]);
+          buffer[offset+7] := ansichar(outbuf[0]);
+       end;
+ end;
+
+const
+ i: word = 1;
+
+
+begin
+  pansicharptr:=pansichar(@i);
+  { Do sanity checking }
+  Assert(sizeof(single)=4);
+  Assert(sizeof(double)=8);
+  if (pansicharptr[0] = #0) then
+    big_endian := True
+  else
+    big_endian := False;
+end.
